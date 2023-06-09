bcc7b1d0e03a702771f3ec7bddb1cb22cd46271d
Aki <aki.ueno@valtech.com>
2023-06-09 16:26:17 +0200

    show selection with button

--- template/custom_table/frontend/src/Custom_Table.tsx
+++ template/custom_table/frontend/src/Custom_Table.tsx
@@ -4,36 +4,14 @@ import {
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
-import React, { Fragment, ReactNode, useMemo } from "react"
-import { MaterialReactTable, type MRT_ColumnDef } from 'material-react-table';
-import { head, isNull, range, zipObject } from "lodash"
-import { type } from "os";
+import React, { Fragment, ReactNode, createRef } from "react"
+import { MaterialReactTable, MRT_RowSelectionState, type MRT_ColumnDef, MRT_TableInstance } from 'material-react-table';
+import { Button } from "@mui/material";
 
 interface State {
   numClicks: number
   isFocused: boolean
 }
-
-interface TableRowProps {
-  rowIndex: number
-  table: ArrowTable
-}
-
-type Column = {
-  accessorKey: string
-  header: string
-};
-
-type Person = {
-  name: {
-    firstName: string;
-    lastName: string;
-  };
-  age: string;
-  address: string;
-  city: string;
-  state: string;
-};
 
 /**
  * This is a React-based component template. The `render()` function is called
@@ -41,14 +19,57 @@ type Person = {
  */
 class CustomTable extends StreamlitComponentBase<State> {
   public state = { numClicks: 0, isFocused: false }
+  tableInstanceRef = createRef<MRT_TableInstance<any>>();
+
+  tableColumns = (table: ArrowTable): MRT_ColumnDef<any>[] => {
+    const headers = table.columnTable.toArray()
+    const tableColumns: any[] = []
+
+    for (let i = 0; i < headers.length; i++) {
+      var header: { [key: string]: any } = {}
+      const name = headers[i][0].toString();
+      header['header'] = name
+      header['accessorKey'] = name
+      tableColumns.push(header)
+      
+    }
+    return tableColumns
+}
+
+
+tableData = (table: ArrowTable): any[] => {
+  const colsNum = table.columns
+  const rowsNum = table.rows
+
+  const headers = table.columnTable.toArray()
+  const tableData = []
+  for (let i = 1; i < rowsNum; i++) {
+    var row: { [key: string]: any } = {}
+    for (let j = 1; j < colsNum; j++) {
+      const element = table.getCell(i, j).content?.toString()
+      const header = headers[j-1][0].toString()
+      if (header != undefined)
+        row[header] = element
+    }
+    tableData.push(row)
+  }
+
+  return tableData
+}
+
+handleRowSelection = () => {
+  const rowSelection = this.tableInstanceRef.current?.getState().rowSelection;
+  console.log('row selected')
+  console.log(rowSelection)
+};
+
 
   public render = (): ReactNode => {
+
     // Arguments that are passed to the plugin in Python are accessible
     // via `this.props.args`. Here, we access the "name" arg.
     const name = this.props.args["name"]
     const data = this.props.args.data;
-    //const columns = this.props.args["columns"]
-    //const data = this.props.args["rows"]
 
     // Streamlit sends us a theme object via props that we can use to ensure
     // that our component has visuals that match the active theme in a
@@ -67,44 +88,6 @@ class CustomTable extends StreamlitComponentBase<State> {
       style.border = borderStyling
       style.outline = borderStyling
     }
-
-    const tableColumns = (table: ArrowTable): MRT_ColumnDef<any>[] => {
-        const colsNum = table.columns
-        const headers = table.columnTable.toArray()
-        const tableColumns: any[] = []
-
-        for (let i = 0; i < headers.length; i++) {
-          var header: { [key: string]: any } = {}
-          const name = headers[i][0].toString();
-          header['header'] = name
-          header['accessorKey'] = name
-          tableColumns.push(header)
-          
-        }
-        return tableColumns
-    }
-
-
-    const tableData = (table: ArrowTable): any[] => {
-      const colsNum = table.columns
-      const rowsNum = table.rows
-
-      const headers = table.columnTable.toArray()
-      const tableData = []
-      for (let i = 1; i < rowsNum; i++) {
-        var row: { [key: string]: any } = {}
-        for (let j = 1; j < colsNum; j++) {
-          const element = table.getCell(i, j).content?.toString()
-          const header = headers[j-1][0].toString()
-          if (header != undefined)
-            row[header] = element
-        }
-        tableData.push(row)
-      }
-
-      return tableData
-    }
-
     
     // Show a button and some text.
     // When the button is clicked, we'll increment our "numClicks" state
@@ -113,10 +96,12 @@ class CustomTable extends StreamlitComponentBase<State> {
     return (
       <Fragment>
         <MaterialReactTable
-          columns={tableColumns(this.props.args.data)}
-          data={tableData(this.props.args.data)}
+          columns={this.tableColumns(data)}
+          data={this.tableData(data)}
           enableRowSelection={true}
+          tableInstanceRef={this.tableInstanceRef}
         />
+        <Button variant="text" onClick={this.handleRowSelection}>bulk edit</Button>
         <span>
         Hello, {name}! &nbsp;
         <button
